f94283520e15e3afbfb39a8cfdde5633a1679220
aburizalpurnama <purnamar9@gmail.com>
2022-02-25 10:05:24 +0700

    Create inputRegisterNasabah

---
+++ nasabah/input.go
@@ -0,0 +1,17 @@
+package nasabah
+
+type InputRegisterNasabah struct {
+	IdKodeBpr    int
+	IdKodeCabang int
+	Nik          string
+	Mobile       string
+	Nama         string
+	Email        string
+	Alamat       string
+	Kelamin      string
+	IdPropinsi   int
+	IdKotaKab    int
+	IdKecamatan  int
+	IdKelurahan  int
+	IdKodePos    int
+}
